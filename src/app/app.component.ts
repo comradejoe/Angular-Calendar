b8c3346c32621f6eeac8c58ae2e583c68dd05804
keerthanajeeva <keerthana@nyl.technology>
2021-11-23 17:55:35 +0530

    Fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { FullCalendarComponent } from "@fullcalendar/angular";
+import { Component, ViewChild } from "@angular/core";
+import { CalendarOptions } from "@fullcalendar/angular";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'full-calendar';
+  @ViewChild("calendar") calendarComponent: FullCalendarComponent;
+  title = "Angular-calendar";
+
+  calendarOptions: CalendarOptions = {
+    initialView: "dayGridMonth",
+    dateClick: this.handleDateClick.bind(this),
+
+    headerToolbar: {
+      left: "prev,today,next",
+      center: "title",
+      right: "dayGridMonth,dayGridWeek,dayGridDay",
+    },
+    views: {
+      dayGridMonth: {
+        buttonText: "Month View",
+      },
+      dayGridWeek: {
+        buttonText: "Week View",
+      },
+      dayGridDay: {
+        buttonText: "Day View",
+      },
+    },
+    navLinks: true,
+    // editable: true,
+    selectable: true,
+    nowIndicator: true,
+    dayMaxEvents: true,
+    events: [
+      { title: "30", date: "2021-07-27" },
+      { title: "14", date: "2021-07-20" },
+      { title: "45", date: "2021-07-21" },
+      { title: "23", date: "2021-07-18" },
+      { title: "15", date: "2021-07-17" },
+    ],
+  };
+
+  onSelect(event) {
+    let calendarApi = this.calendarComponent.getApi();
+    calendarApi.changeView("dayGridDay");
+    console.log(event);
+  }
+
+  handleDateClick(arg) {
+    console.log(arg.dateStr);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
+import { FullCalendarModule } from '@fullcalendar/angular'; // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
+import interactionPlugin from '@fullcalendar/interaction'; // a plugin!
+import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid';
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+  dayGridPlugin,
+  interactionPlugin,
+  listPlugin,
+  timeGridPlugin
+])
+import {MatCardModule} from '@angular/material/card';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +31,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FullCalendarModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    BrowserAnimationsModule
+
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
